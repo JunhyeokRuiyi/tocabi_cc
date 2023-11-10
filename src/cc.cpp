f9c1cf3e19ddf4090b0f5ac0047e203666517fd0
rui_dyrosrui <threeman1@snu.ac.kr>
2023-11-10 14:09:10 +0900

    ppt experiment

--- src/cc.cpp
+++ src/cc.cpp
@@ -16,7 +16,7 @@ CustomController::CustomController(RobotData &rd) : rd_(rd) //, wbc_(dc.wbc_)
         }
         else
         {
-            writeFile.open("/home/dyros/tocabi_ws/src/tocabi_cc/result/iserdata/data_0.csv", std::ofstream::out | std::ofstream::app);
+            writeFile.open("/home/dyros/tocabi_ws/src/tocabi_cc/result/iserdata_ppt/data_250.csv", std::ofstream::out | std::ofstream::app);
         }
         writeFile << std::fixed << std::setprecision(8);
     }
